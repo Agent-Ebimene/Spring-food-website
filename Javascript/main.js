c52f6c03d36b597a9b719247820784e1d245ec0b
Agent Ebimene <ebimeneagent@gmail.com>
2022-09-01 11:12:10 +0100

    Changes Made to the Products

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,19 +1,14 @@
-// document.body.addEventListener("scroll", (e) => {
-//   const firstDeliveryItem = document.getElementById("first-item");
-//   const scrollPosition = window.scrollY;
-//   if (firstDeliveryItem === scrollPosition) {
-//     console.log("Hey");
-//     console.log(scrollPosition);
-//   }
+// const closeBtn = document.getElementById("close-btn");
+// const menuBar = document.querySelector(".nav-icon-box");
+// const sideMenu = document.querySelector(".side-menu");
+// const companyLogo = document.getElementById("company-logo");
+// menuBar.addEventListener("click", () => {
+//   sideMenu.classList.add("visible");
 // });
-
-const closeBtn = document.getElementById("close-btn");
-const menuBar = document.querySelector(".nav-icon-box");
-const sideMenu = document.querySelector(".side-menu");
-menuBar.addEventListener("click", () => {
-  sideMenu.classList.add("visible");
-});
-closeBtn.addEventListener("click", () => {
-  sideMenu.classList.add("remove-side-menu");
-  sideMenu.classList.remove("visible");
-});
+// companyLogo.addEventListener("click", () => {
+//   sideMenu.classList.add("visible");
+// });
+// closeBtn.addEventListener("click", () => {
+//   sideMenu.classList.add("remove-side-menu");
+//   sideMenu.classList.remove("visible");
+// });
